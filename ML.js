fafecf3430821855e728926a28acab90ad300555
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2019-04-05 17:31:33 +0530

    Cleanup of the module loader

--- ML.js
+++ ML.js
@@ -1,33 +1,25 @@
-class moduleLoader{
-    constructor(path)
-    {
-        this.path = []
-        this.functions = []
-        this.path.push(path);
-        this.functions = require(path);
-        this.fkeys = Object.keys(this.functions);
-    }
-    adder(path)
-    {
-        this.path.push(path);
-        this.functions = Object.assign(this.functions,require(path));
-        this.fkeys = Object.keys(this.functions);
-        return this;
-    }
-    funcs()
-    {
-        console.log(this.functions);
-    }
-    Callfunc(func,...args)
-    {
-        var fargs = [];
-        for(var object in arguments)
-        {
-            fargs.push(arguments[object]);
-        }
-        fargs.shift()
-            if(this.fkeys.includes(func) == true)
-            this.functions[func](args);
-    }
+class moduleLoader {
+	constructor(path) {
+		this.path = []
+		this.functions = []
+		this.path.push(path);
+		this.functions = require(path);
+		this.fkeys = Object.keys(this.functions);
+	}
+	adder(path) {
+		this.path.push(path);
+		this.functions = Object.assign(this.functions, require(path));
+		this.fkeys = Object.keys(this.functions);
+		return this;
+	}
+	funcs() {
+		console.log(this.functions);
+	}
+	Callfunc(func, ...args) {
+		if (this.fkeys.includes(func) == true)
+			this.functions[func](...args);
+	}
 
 }
+
+module.exports.ModuleLoader = moduleLoader;
